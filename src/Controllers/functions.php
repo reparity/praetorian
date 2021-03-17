441f56edcff7b3701b960473c4f2736269e328fc
reparity <68840403+reparity@users.noreply.github.com>
2021-03-18 07:42:44 +0800

    feature complete

--- src/Controllers/functions.php
+++ src/Controllers/functions.php
@@ -60,84 +60,23 @@ $app->delete('/adminpage/delete/[{id}]', function ($request, $response, $args) {
     }
     $data = deleteRequest($this->db, $requestId);
     if (empty($data)) {
-        return $this->response->withJson(array('delete' => 'success'), 200);
+        return $this->response->withRedirect("/adminpage", 301);
     }
     return $this->response->withJson(array('delete' => 'fail'), 404);
 });
 
-/** all code below is unedited
- * do not use it
- */
-
-//update product
-$app->put('/product/update/[{id}]', function ($request, $response, $args) {
-    $productId = $args['id'];
-    if (!is_numeric($productId)) {
+//update agent assignment
+$app->put('/adminpage/update/[{id}]', function ($request, $response, $args) {
+    $requestId = $args['id'];
+    if (!is_numeric($requestId)) {
         return $this->response->withJson(array('error' => 'numeric parameter required'), 422);
     }
     $form_data = $request->getParsedBody();
-    $data = updateProduct($this->db, $productId, $form_data);
+    $data = updateAgent($this->db, $requestId, $form_data);
     if ($data <= 0) {
         return $this->response->withJson(array('error' => 'update data fail'), 500);
     }
-    return $this->response->withJson(array('update data' => 'success'), 201);
-});
-
-//delete product
-$app->delete('/product/del/[{id}]', function ($request, $response, $args) {
-    $productId = $args['id'];
-    $data = deleteProduct($this->db, $productId);
-    if (!is_numeric($productId)) {
-        return $this->response->withJson(array('error' => 'numeric parameter required'), 422);
-    }
-    $data = deleteProduct($this->db, $productId);
-    if (empty($data)) {
-        return $this->response->withJson(array('delete' => 'success'), 200);
-    }
-    return $this->response->withJson(array('delete' => 'fail'), 404);
-});
-
-//request table products by condition (price)
-$app->get('/product/price/[{price}]', function ($request, $response, $args){
-
-    $productPrice = $args['price'];
-    if (!is_numeric($productPrice)) {
-        return $this->response->withJson(array('error' => 'numeric parameter required'), 422);
-    }
-    $data = getProductPrice($this->db, $productPrice);
-    if (empty($data)) {
-        return $this->response->withJson(array('error' => 'no data'), 404);
-    }
-    return $this->response->withJson(array('data' => $data), 200);
-});
-
-//update product price by category
-$app->put('/product/update/category/[{cat}]', function ($request, $response, $args) {
-    $productCat = $args['cat'];
-    if (!is_numeric($productCat)) {
-        return $this->response->withJson(array('error' => 'numeric parameter required'), 422);
-    }
-    $form_data = $request->getParsedBody();
-    $data = updateProductPriceByCat($this->db, $productCat, $form_data);
-    if ($data <= 0) {
-        return $this->response->withJson(array('error' => 'update data fail'), 500);
-    }
-    return $this->response->withJson(array('update data' => 'success'), 201);
-});
-
-//delete product by name (fragments)
-$app->delete('/product/del/name/[{name}]', function ($request, $response, $args) {
-    $productName = $args['name'];
-    $data = deleteProductByName($this->db, $productName);
-    if (!is_string($productName)) {
-        return $this->response->withJson(array('error' => 'string parameter required'), 422);
-    }
-    $data = deleteProductByName($this->db, $productName);
-    if (empty($data)) {
-        return $this->response->withJson(array('delete' => 'success'), 200);
-    }
-    return $this->response->withJson(array('delete' => 'fail'), 404);
-    
+    return $this->response->withRedirect("/adminpage", 301);
 });
 
 ?>
--- src/Controllers/functionsProc.php
+++ src/Controllers/functionsProc.php
@@ -55,67 +55,17 @@ function deleteRequest($db, $requestId) {
     $stmt->execute();
 }
 
-/**DO NOT TOUCH */
-
-
-//update existing record - insert ID by url
-function updateProduct($db, $productId, $form_data) {
-    $sql = 'UPDATE products SET name=:name, description=:description, price=:price, category_id=:category_id, created=:created WHERE id=:id';
+//update agent assignment
+function updateAgent($db, $requestId, $form_data) {
+    $sql = 'UPDATE requests SET agent_id=:agentId WHERE id=:r_id';
 
     //echo $sql;
     $stmt = $db->prepare ($sql);
-    $id = (int) $productId;
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->bindParam(':name', $form_data['name']);
-    $stmt->bindParam(':description', $form_data['description']);
-    $stmt->bindParam(':price', $form_data['price']);
-    $stmt->bindParam(':category_id', $form_data['category_id']);
-    $stmt->bindParam(':created', $form_data['created']);
+    $r_id = (int) $requestId;
+    $stmt->bindParam(':r_id', $r_id, PDO::PARAM_INT);
+    $stmt->bindParam(':agentId', $form_data['a_name']);
     $stmt->execute();
     return $stmt->rowCount();
-}
-
-//delete existing record
-function deleteProduct($db, $productId) {
-    $sql = 'DELETE FROM products WHERE id = :id';
-    $stmt = $db->prepare ($sql);
-    $id = (int) $productId;
-    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-    $stmt->execute();
-}
-
-//get product by price
-function getProductPrice($db, $productPrice) {
-    $sql = 'Select p.name, p.description, p.price, c.name as category from products p ';
-    $sql .= 'Inner Join categories c on p.category_id = c.id ';
-    $sql .= 'Where p.price <= :price order by price';
-    $stmt = $db->prepare ($sql);
-    $price = (int) $productPrice;
-    $stmt->bindParam(':price', $price, PDO::PARAM_INT);
-    $stmt->execute();
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-//update existing record prices by category - insert category by url
-function updateProductPriceByCat($db, $productCat, $form_data) {
-    $sql = 'UPDATE products SET price=:price WHERE category_id=:cat';
-
-    //echo $sql;
-    $stmt = $db->prepare ($sql);
-    $cat = (int) $productCat;
-    $stmt->bindParam(':cat', $cat, PDO::PARAM_INT);
-    $stmt->bindParam(':price', $form_data['price']);
-    $stmt->execute();
-    return $stmt->rowCount();
-}
-
-//delete existing record by name
-function deleteProductByName($db, $productName) {
-    $sql = "DELETE FROM products WHERE name LIKE :name";
-    $stmt = $db->prepare ($sql);
-    $name = "%$productName%";
-    $stmt->bindValue(':name', $name);
-    $stmt->execute();
 }
 
 ?>
